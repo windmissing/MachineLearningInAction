56aaa45ec90b885520c135a2724ea28e1a8669a6
He <yina.he@nokia-sbell.com>
2019-12-27 08:55:50 +0800

    page 76

--- Chapter4/bayes.py
+++ Chapter4/bayes.py
@@ -34,3 +34,34 @@ def trainNB0(trainMatrix,trainCategory):
     p1 = p1Num / p1Num.sum()
     pAbusive = category[category==1].shape[0] / category.shape[0]
     return p0, p1, pAbusive
+
+import numpy as np
+def trainNB1(trainMatrix,trainCategory):
+    matrix = np.array(trainMatrix)
+    category = np.array(trainCategory)
+    p0Num = matrix[category==0].sum(axis=0) + np.ones(len(trainMatrix[0]))
+    p0 = p0Num / p0Num.sum()   # 这一次的分母还是很奇怪
+    p1Num = matrix[category==1].sum(axis=0) + np.ones(len(trainMatrix[0]))
+    p1 = p1Num / p1Num.sum()
+    pAbusive = category[category==1].shape[0] / category.shape[0]
+    return np.log(p0), np.log(p1), pAbusive
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p0 = p0Vec[vec2Classify==1].sum() + np.log(1-pClass1)
+    p1 = p1Vec[vec2Classify==1].sum() + np.log(pClass1)
+    return int(p1 > p0)
+
+def testingNB():
+    listOPosts,listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat=[]
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V,p1V,pAb=trainNB1(trainMat,listClasses)
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print (testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print (testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb))
+
