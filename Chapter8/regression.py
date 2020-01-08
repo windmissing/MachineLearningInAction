43763a912dc70cc23207700dae9daeab3e4e788c
He <yina.he@nokia-sbell.com>
2020-01-08 12:25:00 +0800

    183

--- Chapter8/regression.py
+++ Chapter8/regression.py
@@ -62,17 +62,56 @@ def ridgeRegres(xArr,yArr,lam=0.2):
     ws = np.mat(xTx).I.dot(X.T).dot(y)
     return ws.T
 
+def plotRidge():
+    abX,abY=loadDataSet('abalone.txt')
+    ridgeWeights=ridgeTest(abX,abY)
+    import matplotlib.pyplot as plt
+    print (ridgeWeights.shape)
+    plt.plot(ridgeWeights)
+    plt.show()
+
 # 标准化
-def autoNorm(dataSet):
-    max = np.max(dataSet, axis=0)
-    min = np.min(dataSet, axis=0)
-    return ((dataSet -min) / (max-min))
+# 这个作者对标准化（Standardization）、归一化（normalization）、正则化（regularization）这三个术语有什么误解？
+# 一会用normalization一会又用regularization。但它实际上做的是类似于Standardization的事情。
+# 但也只是类似，Standardization公式的分母是标准差，但他用的又是方差。非常奇怪。
+# 另外，对y做预处理似乎也没有意义
+def standardization(dataSet):
+    mean = np.mean(dataSet, axis=0)
+    std = np.std(dataSet, axis=0)
+    return ((dataSet - mean) / std)
 
 def ridgeTest(xArr,yArr):
-    X = autoNorm(np.array(xArr))
-    y = autoNorm(np.array(yArr))
+    X = standardization(np.array(xArr))
+    y = np.array(yArr)
     ws = np.zeros((0, X.shape[1]))
     for i in range(-10, 20):
         w = ridgeRegres(X,y,np.exp(i)).T
         ws = np.vstack([ws, w])
     return ws
+
+def stageWise(xArr,yArr,eps=0.01,numIt=100):
+    # Regularize the data to have 0 mean and unit variance
+    X = standardization(np.array(xArr))
+    y = np.array(yArr)
+    ws = np.zeros(X.shape[1])
+    for iter in range(numIt): # For every iteration:
+        lowestError = np.inf #Set lowestError to +inf
+        for feature in range(X.shape[1]): # For every feature:
+            for sign in [-1, 1]: # For increasing and decreasing:
+                ws_local = ws.copy()
+                ws_local[feature] += eps * sign # Change one coefficient to get a new W
+                err = rssError(y, X.dot(ws_local.T)) # Calculate the Error with new W
+                if err < lowestError: # If the Error is lower than lowestError:
+                    lowestError = err
+                    ws_local_best = ws_local.copy() # set Wbest to the current W
+        ws = ws_local_best.copy() # Update set W to Wbest
+    return ws
+
+def plotStageWise():
+    xArr,yArr=loadDataSet('abalone.txt')
+    testNum = 5000
+    ws = np.zeros((testNum, len(xArr[0])))
+    for i in range(testNum):
+        ws[i,:] = stageWise(xArr,yArr,0.01,i)
+    plt.plot(ws)
+    plt.show()
